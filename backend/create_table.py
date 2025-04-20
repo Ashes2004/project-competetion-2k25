89dd4f67e9785d9bff3df64d7604d3ce14dc9285
Atul <atul.tmsl@gmail.com>
2025-04-20 14:26:17 +0530

    updated the create table file to handle table creation from the backend itself

--- backend/create_table.py
+++ backend/create_table.py
@@ -2,6 +2,9 @@
 from database import Base, engine
 from models.users import User
 from models.startup import Startup
+from models.IPR import IPR
+from models.research import ResearchPaper
+
 
 Base.metadata.create_all(bind=engine)
 print("✅ Tables created!")
